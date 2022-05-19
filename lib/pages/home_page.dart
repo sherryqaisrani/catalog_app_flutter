cb5ba98d857a8489ae25a698ea7a82d43f8e1325
yshaher <hsretra974@gmail.com>
2022-05-19 23:44:02 +0500

    Day9 Task Compelete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,35 @@
+import 'dart:convert';
+
 import 'package:catalog_flutter_application/models/catalog.dart';
 import 'package:catalog_flutter_application/widgets/catalogItem.dart';
 import 'package:catalog_flutter_application/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   var dummyList = List.generate(10, (index) => ItemModel.product[0]);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    final cataData = await rootBundle.loadString('assets/files/catalog.json');
+    final data = jsonDecode(cataData);
+    final productsData = data["product"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
